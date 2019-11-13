8166ea5273b91608c8effc8d8a9426d255bc57e8
peijun333 <unix.043@gmail.com>
2019-11-13 18:30:49 +0900

    ✨Add book modal

--- components/molecules/BookList.vue
+++ components/molecules/BookList.vue
@@ -1,82 +1,113 @@
 <template>
   <!-- TODO: 中央寄せ or 二列にする -->
-  <Card>
-    <List item-layout="vertical">
-      <ListItem v-for="(item, index) in items" :key="index">
-        <ListItemMeta
-          :title="item.title"
-          :description="descriptionLength(item.description)"
-        />
-        <template v-if="item.authors">
-          著者:
-          <span v-for="author in item.authors" :key="author.id">
-            {{ author }}
-          </span>
-        </template>
-        <template v-if="item.publisher">
-          <p>出版社: {{ item.publisher }}</p>
-        </template>
-        <template v-if="item.publishedDate">
-          <p>出版日: {{ item.publishedDate }}</p>
-        </template>
-        <template v-if="flag === 'tsundoku'">
-          <template slot="action">
-            <Progress
-              :percent="item.achievementRate"
-              :stroke-color="['#108ee9', '#87d068']"
-            />
-            <li>
-              <Poptip trigger="hover" placement="right">
-                <Icon type="ios-bookmark-outline" /> しおり:
-                {{ item.currentPageCount }}
-                <div slot="content">
-                  <Input v-model="item.currentPageCount" type="number" />
-                </div>
-              </Poptip>
-            </li>
-            <li>
-              <Icon type="ios-book-outline" /> 総ページ数:
-              {{ item.totalPageCount }}
-            </li>
-            <li>
-              <Icon type="md-create" />
-              <nuxt-link :to="'tsundoku/' + item.id">
-                編集
-              </nuxt-link>
-            </li>
+  <div>
+    <Card>
+      <List item-layout="vertical">
+        <ListItem v-for="(item, index) in items" :key="index">
+          <ListItemMeta
+            :title="item.title"
+            :description="descriptionLength(item.description)"
+          />
+          <template v-if="item.authors">
+            著者:
+            <span v-for="author in item.authors" :key="author.id">
+              {{ author }}
+            </span>
           </template>
-          <br />
-          <br />
-          <p>読書開始日: {{ item.readingStartDate }}</p>
-          <p>読書終了日: {{ item.readingEndDate }}</p>
-          <p>読書終了予定日: {{ item.readingEndExpectedDate }}</p>
-        </template>
-        <template v-if="flag === 'wishlist'">
-          <br />
-          <p>追加日: {{ item.createdAt }}</p>
-          <template slot="action">
-            <!-- TODO: 気になる本からは消えることを警告する -->
-            <li><Icon type="md-add" /> 積み本に追加</li>
-            <li><Icon type="md-create" /> 編集</li>
-            <li><Icon type="ios-remove-circle-outline" /> 削除</li>
+          <template v-if="item.publisher">
+            <p>出版社: {{ item.publisher }}</p>
           </template>
-        </template>
-        <template v-if="flag === 'search'">
-          <template slot="action">
-            <li @click="addTsundoku(item)">
-              <Icon type="md-add" /> 積み本に追加
-            </li>
-            <li @click="addWishlist(item)">
-              <Icon type="md-add" /> 気になる本に追加
-            </li>
+          <template v-if="item.publishedDate">
+            <p>出版日: {{ item.publishedDate }}</p>
           </template>
-        </template>
-        <template slot="extra">
-          <img :src="item.bookImage" style="width: 170px" />
-        </template>
-      </ListItem>
-    </List>
-  </Card>
+          <template v-if="flag === 'tsundoku'">
+            <template slot="action">
+              <Progress
+                :percent="item.achievementRate"
+                :stroke-color="['#108ee9', '#87d068']"
+              />
+              <li>
+                <Poptip trigger="hover" placement="right">
+                  <Icon type="ios-bookmark-outline" /> しおり:
+                  {{ item.currentPageCount }}
+                  <div slot="content">
+                    <Input v-model="item.currentPageCount" type="number" />
+                  </div>
+                </Poptip>
+              </li>
+              <li>
+                <Icon type="ios-book-outline" /> 総ページ数:
+                {{ item.totalPageCount }}
+              </li>
+              <li>
+                <Icon type="md-create" />
+                <nuxt-link :to="'tsundoku/' + item.id">
+                  編集
+                </nuxt-link>
+              </li>
+            </template>
+            <br />
+            <br />
+            <p>読書開始日: {{ item.readingStartDate }}</p>
+            <p>読書終了日: {{ item.readingEndDate }}</p>
+            <p>読書終了予定日: {{ item.readingEndExpectedDate }}</p>
+          </template>
+          <template v-if="flag === 'wishlist'">
+            <br />
+            <p>追加日: {{ item.createdAt }}</p>
+            <template slot="action">
+              <!-- TODO: 気になる本からは消えることを警告する -->
+              <li><Icon type="md-add" /> 積み本に追加</li>
+              <li><Icon type="md-create" /> 編集</li>
+              <li><Icon type="ios-remove-circle-outline" /> 削除</li>
+            </template>
+          </template>
+          <template v-if="flag === 'search'">
+            <template slot="action">
+              <li @click="addTsundoku(item)">
+                <Icon type="md-add" /> 積み本に追加
+              </li>
+              <li @click="addWishlist(item)">
+                <Icon type="md-add" /> 気になる本に追加
+              </li>
+            </template>
+          </template>
+          <template slot="extra">
+            <img :src="item.bookImage" style="width: 170px" />
+          </template>
+        </ListItem>
+      </List>
+    </Card>
+    <!-- TODO: リファクタリング後消す -->
+    <!-- <Modal
+      v-model="modal1"
+      ok-text="登録"
+      cancel-text="キャンセル"
+      title="詳細設定"
+      @on-ok="ok"
+      @on-cancel="cancel"
+    >
+      <span>読書開始日</span><Divider type="vertical" />
+      <DatePicker type="date" placeholder="読書開始日" style="width: 200px" />
+      <Icon type="ios-information-circle-outline" />
+      <br />
+      <span>読書終了予定日</span><Divider type="vertical" />
+      <DatePicker
+        type="date"
+        placeholder="読書終了予定日"
+        style="width: 200px"
+      />
+      <Icon type="ios-information-circle-outline" />
+      <Divider />
+      <span>しおり</span><Divider type="vertical" />
+      <InputNumber v-model="value1" :max="10" :min="1" />
+      <Icon type="ios-information-circle-outline" />
+      <br />
+      <span>総ページ数</span><Divider type="vertical" />
+      <InputNumber v-model="value1" :max="10" :min="1" />
+      <Icon type="ios-information-circle-outline" />
+    </Modal> -->
+  </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
@@ -92,6 +123,8 @@ export default class BookList extends Vue {
   flag!: string
 
   value8: number = 300
+  modal1: boolean = false
+  value1: number = 0
 
   descriptionLength(description: string): string {
     if (description) {
@@ -100,6 +133,13 @@ export default class BookList extends Vue {
         : description
     }
     return description
+  }
+
+  ok() {
+    this.success('tsundoku')
+  }
+  cancel() {
+    this.$Message.info('Clicked cancel')
   }
 
   // TODO: 変数リファクタリング
@@ -144,7 +184,8 @@ export default class BookList extends Vue {
 
   addTsundoku(item: any): void {
     // TODO: dbに追加
-    this.success('tsundoku')
+    this.modal1 = true
+    // this.success('tsundoku')
   }
 }
 </script>
