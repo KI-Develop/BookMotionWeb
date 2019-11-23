304c9c37f430993c43e9dea163b98ee7d4da6ba5
peijun333 <unix.043@gmail.com>
2019-11-23 21:08:03 +0900

    Add user setting page

--- pages/account/setting/index.vue
+++ pages/account/setting/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <p>hoge</p>
+  <div>
+    <Alert show-icon closable style="width:350px; margin:0 auto">
+      テスト版ではユーザー情報の確認のみ可能です。
+    </Alert>
+    <br />
+    <Card style="width:350px; margin:0 auto">
+      <List item-layout="vertical">
+        <ListItem>
+          <ListItemMeta
+            :avatar="this.$store.state.auth.photoURL"
+            :title="this.$store.state.auth.username"
+            :description="this.$store.state.auth.email"
+          />
+        </ListItem>
+      </List>
+    </Card>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
