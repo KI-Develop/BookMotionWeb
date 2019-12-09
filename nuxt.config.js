8fdbc8b836a1ca69bac90322c4c095724825c6f3
peijun333 <unix.043@gmail.com>
2019-12-10 02:04:43 +0900

    Change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   mode: 'spa',
   head: {
-    title: 'BookMotion_Web',
+    title: 'BookMotion',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
