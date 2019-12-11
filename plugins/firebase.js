226d79246e3a9812cd70c89b70fdd1fdbbcbe409
peijun333 <unix.043@gmail.com>
2019-12-11 21:19:56 +0900

    Add analytics

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,10 +1,12 @@
 import * as firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/analytics'
 import config from '~/firebase.config'
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
+  firebase.analytics()
 }
 
 export const authProviders = {
